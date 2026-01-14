94c9b898bb52b7789440207cc1c29ed66270b907
yun413 <wangyunting1024@gmail.com>
2026-01-14 11:35:05 +0800

    260114

--- server.js
+++ server.js
@@ -37,7 +37,6 @@ server.post("/addPortfolio", (req, res) => {
     var file = req.files.workFile;
     var fileName = Date.now() + "_" + file.name;
     
-    // 請務必在你的電腦中手動建立 Public/upload 資料夾
     var savePath = __dirname + "/Public/upload/" + fileName; 
     var webPath = "/upload/" + fileName;
 
@@ -58,7 +57,7 @@ server.post("/addPortfolio", (req, res) => {
     });
 });
 
-// 2. 新增一個 API，讓 admin.html 可以根據類型抓取目前所有的作品名稱
+// 2. 抓取目前所有的作品名稱
 server.get("/getPortfolioNames", (req, res) => {
     PorfolioDB.find({ category: req.query.category }).then(results => {
         res.send(results);
@@ -67,19 +66,19 @@ server.get("/getPortfolioNames", (req, res) => {
 
 // 2.查找
 server.get("/portfolioSearch", (req, res) => {
-    // 使用 findOne 找單一資料
+   
     PorfolioDB.findOne({ 
         category: req.query.category, 
         title: req.query.title 
     }).then(result => {
-        // 如果找不到 result 會是 null
+        
         res.send(result); 
     }).catch(err => {
         res.status(500).send("資料庫查詢失敗");
     });
 });
 
-//3. 處理刪除作品 (包含驗證碼確認) ---
+//3. 處理刪除作品 ---
 server.post("/deletePortfolio", (req, res) => {
     var { category, title, confirmCode } = req.body;
 
@@ -89,7 +88,7 @@ server.post("/deletePortfolio", (req, res) => {
 
     PorfolioDB.remove({ category: category, title: title }, {}).then((numRemoved) => {
         if (numRemoved > 0) {
-            // 刪除成功後直接回到管理頁面
+            
             res.send("<script>alert('作品已永久刪除'); window.location.href='/admin.html';</script>");
         } else {
             res.send("<script>alert('找不到作品，請重新確認'); window.location.href='/admin.html';</script>");
